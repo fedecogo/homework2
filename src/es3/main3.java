a047cb02a49082cc5f4caf224a041b221489a9c4
fedecogo <141748020+fedecogo@users.noreply.github.com>
2023-11-28 16:59:06 +0100

    copsps

--- src/es3/main3.java
+++ src/es3/main3.java
@@ -1,26 +1,3 @@
-package es3;
-
-        import java.util.Scanner;
-
-public class main3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); //parola ins
-
-        do {
-            System.out.println("Inserisci una parola:");
-            String parolaDaDiv = scanner.nextLine();
-            if (parolaDaDiv != (":q")){
-                String[] lettere = parolaDaDiv.split(" ");
-                System.out.println(String.join("",lettere));
-            }
-
-        } while (!parolaDaDiv != (":q"));
-        System.out.println("ciao");
-
-    }}
-
-
-
 import java.util.Scanner;
 
 public class main3 {
